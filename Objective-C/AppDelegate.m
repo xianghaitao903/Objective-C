2da826a74ba689fc322a9f29d5fce22566657ba9
xianghaitao <xianghaitao903@gmail.com>
2016-09-20 10:49:06 +0800

    1

--- Objective-C/AppDelegate.m
+++ Objective-C/AppDelegate.m
@@ -17,12 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   // Override point for customization after application launch.
-//  是撒撒撒
-//  sssssssss
-  //sss
-//  sss
-//  www///
-  //sss
   return YES;
 }
 
