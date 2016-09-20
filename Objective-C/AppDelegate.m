67646edc99f81ec8bea99938c5bf9b58e74d7dde
xianghaitao <xianghaitao903@gmail.com>
2016-09-20 10:20:59 +0800

    test conflict

--- Objective-C/AppDelegate.m
+++ Objective-C/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   // Override point for customization after application launch.
 //  是撒撒撒
-  
+//  sssssssss
   //sss
   //sss
   return YES;
