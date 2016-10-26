533ee42a46adc8cf6047c7534c76a62a2aced5c7
xianghaitao <xianghaitao903@gmail.com>
2016-10-26 08:55:16 +0800

    remove iOSInjectionProject

--- iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/xianghaitao/Documents/XcodeWorkSpace/Objective-C/iOSInjectionProject/BundleContents.m \
-  /tmp/injectionforxcode/BundleInjection.h \
-  /tmp/injectionforxcode/BundleInterface.h \
-  /Users/xianghaitao/Documents/XcodeWorkSpace/Objective-C/iOSInjectionProject/BundleContents.h
